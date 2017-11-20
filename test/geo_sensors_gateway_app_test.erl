0a1020d9f93aeb88dae0c874982ee962e0598446
Yoan Tournade <yoan@ytotech.com>
2017-11-20 13:20:44 +0100

    Found application:ensure_all_started to do exactly what I wanted

--- test/geo_sensors_gateway_app_test.erl
+++ test/geo_sensors_gateway_app_test.erl
@@ -9,17 +9,8 @@ load_configuration_test_() ->
 	[
 		{"Loads configuration from configuration.json file by default",
 		fun() ->
-			% Start all app we required on.
-			% TODO There must be a way to load all that is required.
-			lists:foreach(
-				fun(AppName) ->
-					ok = application:start(AppName)
-				end,
-				[eiconv, jiffy, gen_smtp, nested, idna, unicode_util_compat, hackney]
-			),
-			% TODO How to start it using geo_sensors_gateway:start()?
-			StartApp = application:start(geo_sensors_gateway),
+			{StartAppStatus, _} = application:ensure_all_started(geo_sensors_gateway),
 			% TODO Get internal state.
-			?assertEqual(ok, StartApp)
+			?assertEqual(ok, StartAppStatus)
 		end}
 	].
