414f58b3cb2f520dcd8cdc798771c3cee2e9c100
Yoan Tournade <yoan@ytotech.com>
2018-02-22 02:32:04 +0100

    Better use a gen_server callback module than using process dictionnary, even for testing

--- src/gateway_config_loader_process_dict.erl
+++ src/gateway_config_loader_process_dict.erl
@@ -16,6 +16,10 @@
 %% API
 %%====================================================================
 
+% TODO Even if just for testing, it will be proper as a gen_server callback module.
+% (than maintening state with get and put on the process).
+% https://stackoverflow.com/a/6225749/1956471
+
 -spec load_config() -> #{devices => map(), forwarders => map(), smtp_gateway => map(), users => map()}.
 load_config() ->
 	get("geo_gateway_config").
